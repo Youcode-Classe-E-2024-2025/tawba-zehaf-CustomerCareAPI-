0e82da5fe9387d657acb4ab35b6d2129927ed858
tawbazf <tawbazehaf@gmail.com>
2025-03-24 11:25:15 +0100

    routes for messages

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MessageController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -17,3 +18,8 @@ Route::post('/tickets', [TicketController::class, 'store']);
 Route::get('/tickets/{ticketId}', [TicketController::class, 'show']);
 Route::patch('/tickets/{ticketId}', [TicketController::class, 'updateStatus']);
 Route::delete('/tickets/{ticketId}', [TicketController::class, 'destroy']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/tickets/{ticketId}/messages', [MessageController::class, 'store']);
+    Route::get('/tickets/{ticketId}/messages', [MessageController::class, 'index']);
+});
