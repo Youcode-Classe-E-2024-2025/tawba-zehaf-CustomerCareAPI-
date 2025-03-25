e6dbb35d3092a94d28baf92ec4765071d69cabf7
tawbazf <tawbazehaf@gmail.com>
2025-03-25 11:10:10 +0100

    some views and routes

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,8 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from "./views/login.jsx";
+import Signup from "./views/signup.jsx";
+import Home from "./views/home.jsx";
  const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello world!</div>,
+    path: "/login",
+    element: <login/>,
   },
+  {
+        path: "/signup",
+        element: <Signup/>,
+  },
+  {
+        path: "/home",
+        element: <Home/>,
+  },
+
 ]);
 export default router;
