af7f3837cf04125e2f90c52086c272b1c7d294cd
tawbazf <tawbazehaf@gmail.com>
2025-03-25 12:37:27 +0100

    creation de dossiers dans react

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -2,10 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./views/login.jsx";
 import Signup from "./views/signup.jsx";
 import Home from "./views/home.jsx";
+import NotFound from "./views/notfound.jsx";
  const router = createBrowserRouter([
   {
     path: "/login",
-    element: <login/>,
+    element: <Login/>
   },
   {
         path: "/signup",
@@ -15,6 +16,15 @@ import Home from "./views/home.jsx";
         path: "/home",
         element: <Home/>,
   },
+  {
+        path: "/",
+        element: <Home/>,
+  },
+  {
+    path: "*",
+    element: <NotFound/>,
+},
+
 
 ]);
 export default router;
